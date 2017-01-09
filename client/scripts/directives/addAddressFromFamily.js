ab43349c9ba662b40fd28fd7f49c0cff4a99a16f
jasontenbrink <jason.tenbrink@gmail.com>
2017-01-08 19:37:20 -0600

    added ability to add addresses from other family members addresses

--- client/scripts/directives/addAddressFromFamily.js
+++ client/scripts/directives/addAddressFromFamily.js
@@ -5,64 +5,20 @@ function(AddressService, MemberService, FamilyService){
     scope: {
       nextPage: "&",
       reload: "&",
-      familysAddresses: "="
+      familysAddresses: "=",
+      memberPin: "@"
     },
     templateUrl: 'assets/views/directives/add-address-from-family.html',
     link: function (scope) {
-      console.log('families', scope.familysAddresses);
-      //use that to get pins of family members and get their addresses
-        //in progress.  have a call in familyService set up, but I need to make sure the service works
       var familyService = FamilyService;
-      // familyService.getMembersOfFamilies(scope.familysAddresses.map(function(family){
-      //   return family.familyId;
-      // })).then(function(res){
-      //   console.log(res);
-      // });
-
-      //display those addresses
-      //make those addresses buttons.
-      //If an address is clicked, do a confirmation box, then write the address to the DB, call nextPage()
-      //have a create new address button.  This will call nextPage().
-
       var addressService = AddressService;
-      var memberService = MemberService,
-          familyService = FamilyService;
-
-      var tempAddress = Object.assign({}, scope.address);
-      
-
-      scope.submitRegistration = function (form) {
-        if (form.$valid){
-          if(scope.addressExists){
-            addressService.updateAddress(scope.address).then(
-              function (response) {
-                scope.isDisabled = !scope.isDisabled;
-              });
-          }
-          else{
-            scope.address.pin = memberService.getRegisteringMember().pin;
-
-            //addressService should return a resolved promise if there is no payload on scope.address
-            addressService.postAddress(scope.address).then(
-              function (response) {
-                scope.nextPage();
-              });
-          }
-        }
-      };
-
-      scope.cancel = function(){
-        if(scope.addressExists){
-          Object.assign(scope.address, tempAddress);
-          scope.isDisabled = !scope.isDisabled;
-        }
-        else{
-          scope.nextPage();
-        }
-      };
-
-      scope.skipAddress = function(){
-        scope.nextPage();
+      var memberService = MemberService;
+      scope.addAddress = function (){
+        addressService.postPersonsAddress(scope.memberPin, this.address.address_id)
+        .then(function(res){
+          scope.$emit('addAddressFromFamily'); 
+          scope.reload();
+        })        
       }
     }
   };
--- client/scripts/directives/editableAddressDirective.js
+++ client/scripts/directives/editableAddressDirective.js
@@ -5,22 +5,22 @@ app.directive('editableAddress', ['AddressService', 'MemberService', function(Ad
       address: '=',
       isDisabled: '=',
       addressExists: '=',
-      nextPage: "&"
+      nextPage: "&",
+      reload: "&"
     },
     templateUrl: 'assets/views/directives/editable-address.html',
     link: function (scope) {
       var addressService = AddressService;
       var memberService = MemberService;
       var tempAddress = Object.assign({}, scope.address);
-      console.log('address obj: ', scope.address);
 
       scope.submitRegistration = function (form) {
         if (form.$valid){
           if(scope.addressExists){ //then its an address update
-            console.log('working');
             addressService.updateAddress(scope.address).then(
               function (response) {
                 scope.isDisabled = !scope.isDisabled;
+                scope.reload();
               });
           }
           else{ //its putting in a brand new address
@@ -29,8 +29,8 @@ app.directive('editableAddress', ['AddressService', 'MemberService', function(Ad
             //addressService should return a resolved promise if there is no payload on scope.address
             addressService.postAddress(scope.address).then(
               function (response) {
-                console.log('success from address service', response);
                 scope.$emit('submitAddressForm');
+                scope.reload();
                 scope.nextPage();
               });
           }
